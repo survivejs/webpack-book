f03a831d03abf3b8d5a3aa19e616871d2081f877
Juho Vepsalainen <bebraw@gmail.com>
2017-03-16 15:12:51 +0200

    chore: Add keyword utility

---
+++ utils/keywords.js
@@ -0,0 +1,29 @@
+const retext = require('retext');
+const nlcstToString = require('nlcst-to-string');
+const keywords = require('retext-keywords');
+const glob = require('glob');
+const toVfile = require('to-vfile');
+
+main();
+
+function main() {
+  const corpus = glob.sync('../manuscript/**/*.md').map(
+    name => toVfile.readSync(name, 'utf8').contents
+  ).join('\n');
+
+  retext().use(keywords, { maximum: 30 }).process(
+    corpus, (err, file) => {
+    console.log('Keywords:');
+
+    file.data.keywords.forEach((keyword) => {
+      console.log(nlcstToString(keyword.matches[0].node));
+    });
+
+    console.log();
+    console.log('Key-phrases:');
+
+    file.data.keyphrases.forEach((phrase) => {
+      console.log(phrase.matches[0].nodes.map(nlcstToString).join(''));
+    });
+  })
+}
